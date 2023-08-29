add1b5089b03bcceee49d1e11e2870a9d7bc87ad
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-08-29 15:38:13 -0500

    Add binary search solution

--- repls/linearVsBinarySearch.js
+++ repls/linearVsBinarySearch.js
@@ -1,6 +1,6 @@
 //An exploration of the difference between linear and binary search
 //linear search
-const myArr = [1, 3, 14, 37, 58, 212]
+const myArr = [1, 3, 14, 37, 58, 212];
 
 const linearSearch = (arr, val) => {
   for (let i = 0; i < arr.length; i++){
@@ -10,8 +10,29 @@ const linearSearch = (arr, val) => {
       return false
     }
   }
-}
+};
 
-// console.log(linearSearch(myArr, 7))
-// console.log(linearSearch(myArr, 212))
-// console.log(linearSearch(myArr, 1))
+// console.log(linearSearch(myArr, 7));
+// console.log(linearSearch(myArr, 212));
+// console.log(linearSearch(myArr, 1));
+
+
+const binarySearch = (arr, val) => {
+  let lowerEnd = 0;
+  let upperEnd = arr.length - 1;
+  while (lowerEnd <= upperEnd) {
+  let midIndex = Math.floor(upperEnd + lowerEnd / 2)
+    if (val === arr[midIndex]){
+      return midIndex
+    } else if (val > arr[midIndex]){
+      lowerEnd = midIndex +1
+    } else if (val < arr[midIndex]){
+      upperEnd = midIndex -1
+    }
+  };
+  return 'no match';
+};
+
+console.log(binarySearch(myArr, 7));
+console.log(binarySearch(myArr, 212));
+console.log(binarySearch(myArr, 1));
