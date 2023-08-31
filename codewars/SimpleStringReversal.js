b97fdb6db5bb536f2820563d2ea72abc45c5fe27
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-08-31 16:27:24 -0500

    Add simple string reversal solution

---
+++ codewars/SimpleStringReversal.js
@@ -0,0 +1,53 @@
+//version with comments below!
+
+function solve(str){
+  const split1 = str.split(" ")
+
+  if(split1.length === 1){
+    return str.split("").reverse().join("")
+  } else {
+    const indexes = split1.map(word => {
+      return word.length
+    })
+    indexes.pop()
+    
+    const split = str.split(" ").join("").split("").reverse()
+    
+    const insertSpaces = indexes.reduce((acc, num) => {
+        acc += num
+        split.splice(acc, 0, " ")
+        acc +=1
+        return acc
+      }, 0)
+    return split.join("")
+  }
+};
+
+
+
+// function solve(str){
+//   const split1 = str.split(" ")
+//   //purely to find out how many spaces we need to insert
+
+//   if(split1.length === 1){ //if there's only one word, simply split by character, reverse the array, join by char and return
+//     return str.split("").reverse().join("")
+//   } else { //if there's any spaces to handle:
+//     //first get an array of the lengths of the words, so we can deduce the index of space
+//     const indexes = split1.map(word => {
+//       return word.length
+//     })
+//     indexes.pop() //remove the last item, since we won't need a space at the end
+    
+//     const split = str.split(" ").join("").split("").reverse()
+//     //split the original string without the spaces and reverse it
+//     const split2 = indexes.reduce((acc, num) => {
+//         acc += num
+//         split.splice(acc, 0, " ")
+//         acc +=1
+//         return acc
+//       }, 0)
+//     //reintroduce the spaces -line 19 adds one to account for the space you're adding
+//   return split.join("")
+//     //return the joined string!
+//   }
+// };
