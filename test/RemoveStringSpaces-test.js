bcbb2511c3830733b21f2cfea8e146c7fa0a1ec9
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-09-03 21:50:54 -0500

    Add tests for NoSpace1 function

--- test/RemoveStringSpaces-test.js
+++ test/RemoveStringSpaces-test.js
@@ -26,5 +26,32 @@ describe('noSpace0', () => {
     const str = '   She         sells seashells       by the seashore. ';
     const noSpaces0 = noSpace0(str);
     expect(noSpaces0).to.equal('Shesellsseashellsbytheseashore.');
-  })
+  });
+});
+
+describe('noSpace1', () => {
+  it('should be a function', () => {
+    expect(noSpace1).to.be.a('function');
+  });
+
+  it('should return a string', () => {
+    const str = 'She sells seashells by the seashore.';
+    const noSpaces0 = noSpace1(str);
+    expect(noSpaces0).to.be.a('string');
+  });
+
+  it('should return a string with no spaces', () => {
+    const str = 'She sells seashells by the seashore.';
+    const str1 = '8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd';
+    const noSpaces0 = noSpace1(str);
+    const noSpaces1 = noSpace1(str1);
+    expect(noSpaces0).to.equal('Shesellsseashellsbytheseashore.');
+    expect(noSpaces1).to.equal('88Bifk8hB8BB8BBBB888chl8BhBfd');
+  });
+
+  it('should return a string with no spaces if multiple spaces in a row', () => {
+    const str = '   She         sells seashells       by the seashore. ';
+    const noSpaces0 = noSpace1(str);
+    expect(noSpaces0).to.equal('Shesellsseashellsbytheseashore.');
+  });
 });
