383d311cd8097a5fad00208566c69a5227bd9832
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-09-03 20:06:04 -0500

    Refactor solution for Remove String Space

--- codewars/RemoveStringSpaces.js
+++ codewars/RemoveStringSpaces.js
@@ -1,6 +1,13 @@
+//REFACTORED SOLUTION
+function noSpace(x){
+  return x.split(" ").join("");
+};
+
+
+// ORIGINAL SOLUTION
 function noSpace(x){
   var y = "";
-  
+
   for (var i = 0; i < x.length; i++){
     if (x[i] !== " "){
       y = y + x[i];
