c287e873208241878aa8752d63e5edde5429a4ad
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-09-03 21:31:21 -0500

    Reorganize comments

--- codewars/RemoveStringSpaces.js
+++ codewars/RemoveStringSpaces.js
@@ -1,8 +1,4 @@
-/* The original solution creates a new variable and reassigns it, making it's space complexity higher. 
-Both solutions loop through the string (x) passed into the function, making the time complexity both O(n) notation.
-The refactored solution is easier to read, putting it just over the finish line as our winner.
-I look forward to learning regular expressions and revisiting this problem again in the future.
-*/
+//Write a function that takes in a string, removes the spaces, and returns the resulting string.
 
 //REFACTORED SOLUTION
 function noSpace1(x){
@@ -23,5 +19,14 @@ function noSpace0(x){
   return y;
 
 };
+
+
+/* 
+ANALYSIS
+The original solution creates a new variable and reassigns it, making it's space complexity higher. 
+Both solutions loop through the string (x) passed into the function, making the time complexity both O(n) notation.
+The refactored solution is easier to read, putting it just over the finish line as our winner.
+I look forward to learning regular expressions and revisiting this problem again in the future.
+*/
 
 module.exports = { noSpace0, noSpace1 };
