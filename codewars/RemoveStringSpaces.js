a2e32eeebe882a34710d123de9b898e8f2cc6984
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-09-03 20:38:05 -0500

    Add notes comparing the time and space complexity of the solutions

--- codewars/RemoveStringSpaces.js
+++ codewars/RemoveStringSpaces.js
@@ -1,11 +1,17 @@
+/* The original solution creates a new variable and reassigns it, making it's space complexity higher. 
+Both solutions loop through the string (x) passed into the function, making the time complexity both O(n) notation.
+The refactored solution is easier to read, putting it just over the finish line as our winner.
+I look forward to learning regular expressions and revisiting this problem again in the future.
+*/
+
 //REFACTORED SOLUTION
-function noSpace(x){
+function noSpace1(x){
   return x.split(" ").join("");
 };
 
 
 // ORIGINAL SOLUTION
-function noSpace(x){
+function noSpace0(x){
   var y = "";
 
   for (var i = 0; i < x.length; i++){
