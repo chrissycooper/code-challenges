5cc2847d7f7e3f692bf3c79a25a5fcb2fad35227
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-09-08 18:11:50 -0500

    Add more tests for filter Null Values

--- test/filterNullValues-test.js
+++ test/filterNullValues-test.js
@@ -6,4 +6,21 @@ describe('filterNullValues', () => {
     expect(dataArray).to.be.an('array');
     expect(filterNullValues).to.be.a('function');
   })
+
+  it('Should output an array of objects', () => {
+    const filteredArr = filterNullValues(dataArray)
+    expect(filteredArr).to.be.an('array');
+    expect(filteredArr[0]).to.be.an('object');
+  })
+
+  it('Should not have any null key value pairs in the return value', () => {
+    const filteredArr = filterNullValues(dataArray)
+    const values = Object.values(filteredArr)
+
+    values.forEach((value) => {
+      expect(value).to.not.be('null')
+    })
+    expect(filteredArr).to.be.an('array');
+    expect(filteredArr[0]).to.be.an('object');
+  })
 })
