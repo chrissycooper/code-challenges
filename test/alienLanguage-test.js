4ecda7f55f8ae4a37d350022759ef7891b9d86d9
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-09-12 22:15:13 -0500

    Add return test to alienLanguage

--- test/alienLanguage-test.js
+++ test/alienLanguage-test.js
@@ -5,4 +5,9 @@ describe('alienLanguage', () => {
   it('Should be a function', () => {
     expect(alienLanguage).to.be.a('function');
   });
+
+  it('Should return a string', () => {
+    const alienSays = alienLanguage("My name is John")
+    expect(alienSays).to.be.a('string');
+  });
 });
