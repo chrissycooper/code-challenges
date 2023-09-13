4ebfcfb2d1920ade0608a5c740120af097cf37d3
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-09-13 11:22:02 -0500

    Add edgecases

--- test/fixStringCase-test.js
+++ test/fixStringCase-test.js
@@ -16,18 +16,35 @@ describe('fixStringCase', () => {
     expect(strawberry).to.equal('STRAWBERRY')
   })
 
-  it('Should return a lowercase string if the input string has an equal number of upper and lower', () => {
+  it('Should return a lowercase string if the input has an equal number of upper and lower', () => {
     const strawberry = solve('STRAWberry')
     expect(strawberry).to.equal('strawberry')
   })
 
-  it('Should return a lowercase string if the input string has more lowercase than uppercase', () => {
+  it('Should return a lowercase string if the input has more lower than uppercase', () => {
     const strawberry = solve('STRAwberry')
     expect(strawberry).to.equal('strawberry')
+
+    const blueberry = solve('bLuEbErRy')
+    expect(blueberry).to.equal('blueberry')
   })
 
-  it('Should return an uppercase string if the input string has more uppercase than lowercase', () => {
+  it('Should return an uppercase string if the input has more upper than lowercase', () => {
     const strawberry = solve('straWBERRY')
     expect(strawberry).to.equal('STRAWBERRY')
+
+    const blueberry = solve('BLUeBERRY')
+    expect(blueberry).to.equal('BLUEBERRY')
   })
+
+  it('Should return a string if passed a special characters in a string', () => {
+    const strawberry = solve('23984723948')
+    expect(strawberry).to.equal('23984723948')
+    const strawb3rry = solve('%^*)()@kjsdfhHHHHHHHHHHHHHHHHH')
+    expect(strawb3rry).to.equal('%^*)()@KJSDFHHHHHHHHHHHHHHHHHH')
+    const strawb3rry2 = solve('%^*)()@kjsdfhHHHHHH')
+    expect(strawb3rry2).to.equal('%^*)()@kjsdfhhhhhhh')
+  })
+  
+  
 });
