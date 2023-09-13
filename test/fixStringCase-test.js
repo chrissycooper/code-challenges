b20ab8c254fd2153ae753003085768bb5a31fd6d
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-09-13 09:55:56 -0500

    Add first test

--- test/fixStringCase-test.js
+++ test/fixStringCase-test.js
@@ -1,2 +1,8 @@
 const expect = require('chai').expect;
+const { solve } = require('../codewars/fixStringCase.js')
 
+describe('fixStringCase', () => {
+  it('Should be a function', () => {
+    expect(solve).to.be.a('function')
+  })
+});
