640a36949ddddff80ae66d0516240a1ca8c19b50
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-09-15 20:42:49 -0500

    Add tests for simple string reversal

--- test/simpleStringReversal-test.js
+++ test/simpleStringReversal-test.js
@@ -4,8 +4,20 @@ const { reverseString } = require('../codewars/SimpleStringReversal.js');
 describe('Simple String Reversal', () => {
   it('Should be a function', () => {
     expect(reverseString).to.be.a('function');
-  })
-  it('Should be a function', () => {
-    expect(reverseString).to.be.a('function');
-  })
-})
+  });
+
+  it('Should be able to reverse a simple string', () => {
+    const reverse = reverseString('vampire')
+    expect(reverse).to.equal('eripmav');
+  });
+
+  it('Should be able to maintain spaces', () => {
+    const reverse = reverseString('vampire hunters')
+    expect(reverse).to.equal('sretnuh eripmav');
+  });
+
+  it('Should be able to maintain spaces - more complex', () => {
+    const reverse = reverseString('buffy the vampire slayer')
+    expect(reverse).to.equal('reyal ser ipmaveh tyffub');
+  });
+});
