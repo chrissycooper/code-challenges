7115be1ff47ed2c693564a26ff384e05d7c37821
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-09-18 20:49:15 -0500

    Alien language tests

--- test/alienLanguage-test.js
+++ test/alienLanguage-test.js
@@ -7,7 +7,22 @@ describe('alienLanguage', () => {
   });
 
   it('Should return a string', () => {
-    const alienSays = alienLanguage("My name is John")
+    const alienSays = alienLanguage("My name is John");
     expect(alienSays).to.be.a('string');
+  });
+
+  it('Should return a properly cased string', () => {
+    const alienSays = alienLanguage('My name is John');
+    expect(alienSays).to.equal('My NAMe Is JOHn');
+  });
+
+  it('Should return a properly cased string pt.2', () => {
+    const alienSays = alienLanguage('this is an example');
+    expect(alienSays).to.equal('THIs Is An EXAMPLe');
+  });
+
+  it('Should return a properly cased string - all caps', () => {
+    const alienSays = alienLanguage('HELLO WORLD');
+    expect(alienSays).to.equal('HELLo WORLd');
   });
 });
