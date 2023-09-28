56d73c1a82ea4c0406ccb09ab1446eefacd36303
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-09-28 10:49:43 -0500

    Add solution to mumbling

--- codewars/mumbling.js
+++ codewars/mumbling.js
@@ -7,7 +7,18 @@
 // The parameter of accum is a string which includes only letters from a..z and A..Z.
 
 function accum(s) {
-
-};
+	// With each letter, capitalize the first one, and duplicate it, one more than the last - with a dash
+  const split = s.split('')
+  const accum = split.reduce((acc, cv, index) => {
+    //add the same number of strings as the index
+   let string = cv.toUpperCase()
+   for (var i=0; i < index; i++) {
+     string += cv.toLowerCase()
+   }
+   index > 0 ? acc += '-' + string : acc += string
+    return acc
+  }, '')
+  return accum
+}
 
 module.exports = { accum };
